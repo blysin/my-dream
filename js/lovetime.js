ba7f069243b3f8e160600de760d2a66be398d8aa
戴少坤 <blysin@163.com>
2021-10-06 21:33:26 +0800

    添加月

--- js/lovetime.js
+++ js/lovetime.js
@@ -23,7 +23,11 @@ function lovetime() {
     var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes)
     var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes *
         minutes) / seconds)
-    document.getElementById("lovetime").innerHTML = "我们已经在一起 " + diffYears + "年" + diffDays + "天" +
+
+    var diffMonth = Math.floor(diffDays / 30)
+    diffDays = diffDays - diffMonth * 30
+
+    document.getElementById("lovetime").innerHTML = "我们已经在一起 " + diffYears + "年" +diffMonth+'月'+ diffDays + "天" +
         diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒啦"
 }
 lovetime()
